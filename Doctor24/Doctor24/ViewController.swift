cedec5f74efa856d91b6584567a5e0ad60230e7e
JHH <wjdgo813@naver.com>
2020-01-05 22:18:53 +0900

    git init

---
+++ Doctor24/Doctor24/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Doctor24
+//
+//  Created by Haehyeon Jeong on 2020/01/05.
+//  Copyright © 2020 JHH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
